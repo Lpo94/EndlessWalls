c4aa671914da5c6d72dd4b024ce90c646e675d6f
Lars-Peter Olsen <lars.peter.olsen@gmail.com>
2017-04-10 14:10:41 +0200

    2nd Commit (Please don't fuck shit up)

---
+++ app/src/main/java/com/example/lars_peter/endlesswalls/Constants.java
@@ -0,0 +1,11 @@
+package com.example.lars_peter.endlesswalls;
+
+/**
+ * Created by SharkGaming on 07/04/2017.
+ */
+
+public abstract class Constants
+{
+    public static int SCREEN_WIDTH;
+    public static int SCREEN_HEIGHT;
+}
---
+++ app/src/main/java/com/example/lars_peter/endlesswalls/LevelObject.java
@@ -0,0 +1,13 @@
+package com.example.lars_peter.endlesswalls;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by SharkGaming on 07/04/2017.
+ */
+
+public interface LevelObject
+{
+    void draw(Canvas _canvas);
+    void update();
+}
